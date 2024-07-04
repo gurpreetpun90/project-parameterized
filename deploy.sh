8e1ff44dcedb6372489914d8744886662656eaab
gurpreetpun90 <gsnagpal90@gmail.com>
2024-07-04 14:08:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-@echo off
-echo Deploy script from GitHub repository is being used.
-echo Deploying build %1
-REM Add your deployment steps here
+#!/bin/bash
+echo "Deploy script from GitHub repository is being used."
+echo "Deploying build $1"
+# Add your deployment steps here
